c402e4c164a7c92ead28daa135e2cfb0dfa68e17
croftn <nyatcroft@gmail.com>
2024-01-17 11:43:42 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using Mission2Assignment;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to the dice throwing simulator!\n");
+
+        Console.WriteLine("How many dice rolls would you like to simulate? ");
+        string userInput = System.Console.ReadLine();
+     
+        int numberOfRolls = int.Parse(userInput);
+
+        RollDice roll = new RollDice();
+
+        int[] gameResults = roll.RollTheDice(numberOfRolls);
+
+        Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
+        Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
+        Console.WriteLine("Total number of rolls = " + numberOfRolls + ".\n\n");
+
+        for (int iCount = 2; iCount <= 12; iCount++)
+        {
+            int percentage = gameResults[iCount - 2] * 100 / numberOfRolls;
+            string totalPercentage = new string('*', percentage);
+
+            Console.WriteLine(iCount + ": " + totalPercentage);
+
+        }
+
+        Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
+
+
+    }
+}
+
---
+++ RollDice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2Assignment
+{
+    internal class RollDice
+    {
+        private Random random;
+
+        public RollDice()
+        {
+            random = new Random();
+        }
+
+        public int[] RollTheDice(int numberOfRolls)
+        {
+            int[] gameResults = new int[11];
+            for (int iCount = 0; iCount < numberOfRolls; iCount++)
+            {
+                int dice1 = random.Next(1, 7);
+                int dice2 = random.Next(1, 7);
+                int sum = dice1 + dice2;
+
+                gameResults[sum - 2]++;
+            }
+
+            return gameResults;
+        }
+
+    }
+}
