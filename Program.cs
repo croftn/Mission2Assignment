8b0d12afa6c7610cb78a247f4ea4e0ef7cc09a4a
croftn <nyatcroft@gmail.com>
2024-01-17 22:53:14 -0700

    Adding comments

--- Program.cs
+++ Program.cs
@@ -1,30 +1,43 @@
-﻿using Mission2Assignment;
+﻿// Author: Nya Croft
+// Section 004
+
+
+// Import namespace from RollDice
+using Mission2Assignment;
+
+// The main class of the program
 public class Program
 {
+    // The entry point of the program
     private static void Main(string[] args)
     {
         Console.WriteLine("Welcome to the dice throwing simulator!\n");
 
+        // Ask user how mnay rolls and store the value
         Console.WriteLine("How many dice rolls would you like to simulate? ");
         string userInput = System.Console.ReadLine();
-     
         int numberOfRolls = int.Parse(userInput);
 
+        // Create an instance of the RollDice class
         RollDice roll = new RollDice();
 
+        // Call the RollTheDice method to get the dice rolling game results 
         int[] gameResults = roll.RollTheDice(numberOfRolls);
 
+        // Display dice rolling results
         Console.WriteLine("\nDICE ROLLING SIMULATION RESULTS");
         Console.WriteLine("Each \"*\" represents 1% of the total number of rolls.");
-        Console.WriteLine("Total number of rolls = " + numberOfRolls + ".\n\n");
+        Console.WriteLine("Total number of rolls = " + numberOfRolls + ".\n");
 
+        // Loop through the gameResults array
         for (int iCount = 2; iCount <= 12; iCount++)
         {
+            // Calculate the percentage each number sum was rolled
             int percentage = gameResults[iCount - 2] * 100 / numberOfRolls;
+
+            // Display histogram using a "*" for each 1% of the total rolls for each number
             string totalPercentage = new string('*', percentage);
-
             Console.WriteLine(iCount + ": " + totalPercentage);
-
         }
 
         Console.WriteLine("\nThank you for using the dice throwing simulator. Goodbye!");
--- RollDice.cs
+++ RollDice.cs
@@ -1,33 +1,53 @@
-﻿using System;
+﻿// Author: Nya Croft
+// Section 004
+
+
+// Import namespaces
+using System;
 using System.Collections.Generic;
 using System.Diagnostics.SymbolStore;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+// Define namespace
 namespace Mission2Assignment
 {
+    // Declare class
     internal class RollDice
     {
+        // Holds a random instance for dice rolling
         private Random random;
 
+        // RollDice class constructor
         public RollDice()
         {
+            // Initialize the random instance
             random = new Random();
         }
 
+        // Method to simulate rolling the dice a specific number of times based on the user input 
         public int[] RollTheDice(int numberOfRolls)
         {
+            // Array to store the results of each possible dice roll sum
             int[] gameResults = new int[11];
+
+            // Loop simulates each dice roll
             for (int iCount = 0; iCount < numberOfRolls; iCount++)
             {
+                // Generate random values between 1 and 6 (two 6-sided dice)
                 int dice1 = random.Next(1, 7);
                 int dice2 = random.Next(1, 7);
+
+                // Calculate the sum of the two dice
                 int sum = dice1 + dice2;
 
+                // Increment the corresponding position in the array based on the sum 
                 gameResults[sum - 2]++;
             }
 
+            // Return the array containing the results of the dice rolls
             return gameResults;
         }
 
